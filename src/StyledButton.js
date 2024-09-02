40ac45a40c0e39c2232f6ad2952d6c69bce540b0
DavSan <david.sanchez.martinez@outlook.com>
2024-09-02 15:51:41 +0100

    Update StyledButton.js

--- src/StyledButton.js
+++ src/StyledButton.js
@@ -1,23 +1,27 @@
 import React from 'react'
 
+
+
 const StyledButton = () => {
-    const StyledHeading = {
+    const isDisabled = false
+
+    const headingStyle = {
         textAlign: 'center',
         color: 'darkgreen',
-        backgroundColor: 'darkyellow'
+        backgroundColor: 'yellow'
     }
-    const StyledButton = {
+    const buttonStyle = {
         padding: '8px 12px',
-        border: '2px solid black'
-        borderRadius: '8px' 
+        border: '2px solid black',
+        borderRadius: '8px', 
         color: 'darkgreen',
-        backgroundColor: 'darkyellow'
+        backgroundColor: 'yellow'
     }
   
     return (
     <div>
-        <h1>I'm the One Who Knocks</h1>
-        <button>Knock</button>      
+        <h1 style={headingStyle}>I'm the One Who Knocks</h1>
+        <button style={buttonStyle} disable={isDisabled} className='knockBtn'>Knock</button>      
     </div>
   )
 }
