1468c070216901d8962bfcefc528ab582977e75a
DavSan <david.sanchez.martinez@outlook.com>
2024-09-02 15:36:59 +0100

    Update StyledButton.js

--- src/StyledButton.js
+++ src/StyledButton.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const StyledButton = () => {
-  return (
+    const StyledHeading = {
+        textAlign: 'center',
+        color: 'darkgreen',
+        backgroundColor: 'darkyellow'
+    }
+    
+  
+    return (
     <div>
         <h1>I'm the One Who Knocks</h1>
         <button>Knock</button>      
